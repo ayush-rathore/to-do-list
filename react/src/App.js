c9b7c3f356aaa320f729ca88bd7e693621d3b7d7
ayush-rathore <aayushh79@gmail.com>
2021-10-12 18:06:32 +0530

    fix: Landing

--- react/src/App.js
+++ react/src/App.js
@@ -3,37 +3,36 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Login from "./components/Login";
 import Signup from "./components/Signup";
 import Home from "./components/Home";
-import Landing from "./components/landing";
 
 const App = () => {
-    const [user, setUserState] = useState({});
-    useEffect(() => {
-        let savedUser = localStorage.getItem("user");
-        console.log(savedUser);
-        if (savedUser && Object.keys(user).length === 0) {
-            setUserState(JSON.parse(savedUser).user);
-            console.log(user);
-        }
-    }, [user]);
+	const [user, setUserState] = useState({});
+	useEffect(() => {
+		let savedUser = localStorage.getItem("user");
+		console.log(savedUser);
+		if (savedUser && Object.keys(user).length === 0) {
+			setUserState(JSON.parse(savedUser).user);
+			console.log(user);
+		}
+	}, [user]);
 
-    return (
-        <Router>
-            <Switch>
-                <Route path={"/login"} exact>
-                    <Login setUserState={setUserState} />
-                </Route>
-                <Route path={"/signup"} exact>
-                    <Signup setUserState={setUserState} />
-                </Route>
-                <Route path={"/home"}>
-                    <Home user={user} />
-                </Route>
-                <Route path={"/"}>
-                    <Landing />
-                </Route>
-            </Switch>
-        </Router>
-    );
+	return (
+		<Router>
+			<Switch>
+				<Route path={"/login"} exact>
+					<Login setUserState={setUserState} />
+				</Route>
+				<Route path={"/signup"} exact>
+					<Signup setUserState={setUserState} />
+				</Route>
+				<Route path={"/home"}>
+					<Home user={user} />
+				</Route>
+				<Route path={"/"}>
+					<Login />
+				</Route>
+			</Switch>
+		</Router>
+	);
 };
 
 export default App;
