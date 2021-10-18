cf8806dcb576e7326e1a304918727144b42bfdf9
ayush-rathore <aayushh79@gmail.com>
2021-10-18 19:51:00 +0530

    update user model

--- node/models/user.js
+++ node/models/user.js
@@ -1,20 +1,16 @@
 const mongoose = require("mongoose");
 const User = new mongoose.Schema({
-    id: {
-        type: mongoose.SchemaTypes.ObjectID,
-    },
-    name: {
-        type: String,
-        required: true,
-    },
-    email: {
-        type: String,
-        required: true,
-    },
-    password: {
-        type: String,
-        required: true,
-    },
+	id: {
+		type: mongoose.SchemaTypes.ObjectID,
+	},
+	userName: {
+		type: String,
+		required: true,
+	},
+	password: {
+		type: String,
+		required: true,
+	},
 });
 
 module.exports = mongoose.model("user", User, "user");
