357acbb105a9a57d7d53e1fb03a2a9bcecfcf644
ayush-rathore <aayushh79@gmail.com>
2021-10-21 18:14:09 +0530

    Clean app.js

--- node/app.js
+++ node/app.js
@@ -1,31 +1,14 @@
+require("dotenv").config();
+require("./config/database").connect();
 const express = require("express");
-const app = express();
-const mongoose = require("mongoose");
-const router = require("./routes/userRoute");
-const todo = require("./routes/todoRoute");
 const cors = require("cors");
 
-app.use(express.urlencoded({ extended: false }));
+const app = express();
+const router = require("./routes/route");
+
 app.use(express.json());
 app.use(cors());
 
-mongoose
-    .connect("mongodb://localhost:27017/to-do", {
-        useUnifiedTopology: true,
-        useNewUrlParser: true,
-    })
-    .then(() => {
-        console.info("--- Database connected succesfully ---");
-    })
-    .catch(() => {
-        console.error("--- Mongodb connection failed ---");
-    });
-
-const PORT = 8080;
-
-app.listen(PORT, () => {
-    console.info(`--- Server listening on PORT ${PORT} ---`);
-});
-
 app.use(router);
-app.use("/todo", todo);
+
+module.exports = app;
