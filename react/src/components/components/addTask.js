289d56987383373ab306f5add18ee80f900274bf
ayush-rathore <aayushh79@gmail.com>
2021-10-21 19:10:11 +0530

    Add task component

---
+++ react/src/components/components/addTask.js
@@ -0,0 +1,60 @@
+import React, { useState } from "react";
+import Axios from "axios";
+import "../css/task.css";
+
+const AddTask = (props) => {
+	const [task, setTask] = useState({});
+	const { tasks, setTasks, user, setScreenType } = props;
+
+	const onAddTask = async (event) => {
+		event.preventDefault();
+		await Axios.post("http://localhost:8080/todo/", {
+			...task,
+			userId: user._id,
+		})
+			.then(({ data: addedTask }) => {
+				window.alert("Task Added!");
+				setTasks([...tasks, addedTask]);
+				setTimeout(() => {
+					setScreenType("SHOW");
+				}, 1000);
+			})
+			.catch((error) => {
+				console.error(error);
+			});
+	};
+	return (
+		<div className={"task"}>
+			<h2>Let's create some tasks for you...</h2>
+			<hr />
+			<form>
+				<label>Task Name</label> <br /> <br />
+				<input
+					type={"text"}
+					placeholder={"Enter Task Name"}
+					onChange={(event) => {
+						setTask({ ...task, taskName: event.target.value });
+					}}
+				/>{" "}
+				<br /> <br />
+				<label>Task Description</label> <br /> <br />
+				<input
+					type={"text"}
+					placeholder={"Enter Task Description"}
+					onChange={(event) => {
+						setTask({
+							...task,
+							taskDescription: event.target.value,
+						});
+					}}
+				/>{" "}
+				<br /> <br />
+			</form>
+			<button onClick={onAddTask}>
+				{" "}
+				<i class="far fa-address-card"></i> Add Task
+			</button>
+		</div>
+	);
+};
+export default AddTask;
