ee11f5f33ab98bd0b84613d08d4046c181b6eee6
ayush-rathore <aayushh79@gmail.com>
2021-10-21 18:13:23 +0530

    Add database config

---
+++ node/config/database.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+const { MONGO_URL } = process.env;
+
+exports.connect = () => {
+	mongoose
+		.connect(MONGO_URL, {
+			useNewUrlParser: true,
+			useUnifiedTopology: true,
+		})
+		.then(() => {
+			console.info("--- Database connected succesfully ---");
+		})
+		.catch(() => {
+			console.error("--- Mongodb connection failed ---");
+		});
+};
