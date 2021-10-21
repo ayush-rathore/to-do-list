e832d362fe2803b6a1954feaaf1043554f5fcdce
ayush-rathore <aayushh79@gmail.com>
2021-10-21 18:12:53 +0530

    Remove user routes

--- node/routes/userRoute.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express();
-const userController = require("../controller/userController");
-
-router.post("/signup", userController.signup);
-
-router.post("/login", userController.login);
-
-module.exports = router;
