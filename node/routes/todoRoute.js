1cf6342d5cbd7330fd7502f3331ec92d6b8febdb
ayush-rathore <aayushh79@gmail.com>
2021-10-21 18:12:43 +0530

    Remove ToDo routes

--- node/routes/todoRoute.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express();
-const taskController = require("../controller/todoController");
-
-router.post("/", taskController.addTask);
-router.get("/user/:userId", taskController.getTaskByUserId);
-router.delete("/task/:taskId", taskController.removeTask);
-module.exports = router;
