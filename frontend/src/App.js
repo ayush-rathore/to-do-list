6d57dd0d90b1267531fdbe7782135a0b6896c872
ayush-rathore <aayushh79@gmail.com>
2021-10-25 22:03:24 +0530

    Fix null user object

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,18 +2,20 @@ import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Login from "./components/pages/Login";
 import Signup from "./components/pages/Signup";
-import "./app.css";
 import Home from "./components/pages/Home";
+import "./app.css";
 
 const App = () => {
 	const [user, setUserState] = useState({});
 	useEffect(() => {
 		let savedUser = localStorage.getItem("user");
-		console.log(savedUser);
-		// if (savedUser && Object.keys(user).length === 0) {
-		// 	setUserState(JSON.parse(savedUser).user);
-		// 	console.log(user);
-		// }
+		if (
+			savedUser &&
+			(user === undefined || Object.keys(user).length === 0)
+		) {
+			setUserState(JSON.parse(savedUser));
+			console.log(user);
+		}
 	}, [user]);
 
 	return (
