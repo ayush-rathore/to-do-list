a7cd49fe630ef656a094d361580195478fa0833c
ayush-rathore <aayushh79@gmail.com>
2021-10-25 20:10:11 +0530

    Update routes

--- node/routes/route.js
+++ node/routes/route.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express();
 
 const userController = require("../controller/userController");
-const taskController = require("../controller/todoController");
+const taskController = require("../controller/taskController");
 
 // User Routes
 router.post("/user/signup", userController.signup);
@@ -10,7 +10,8 @@ router.post("/user/login", userController.login);
 router.get("/users", userController.users);
 
 //To Do Routes
-router.post("/todo/add", taskController.addTask);
-router.get("/todo/getTask", taskController.getTask);
+router.post("/todo/addTask", taskController.addTask);
+router.get("/todo/getTask/:userID", taskController.getTask);
+router.get("/todo/removeTask/:taskID", taskController.removeTask);
 
 module.exports = router;
